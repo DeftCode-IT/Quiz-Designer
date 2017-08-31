72045e4acb861486ffdb830dbeaee57c12e5e738
DeftCode <admin@deftcode.it>
2017-08-31 18:50:24 +0200

    [QD-5] Fixing problem with path

--- server/src/common/jwt/index.js
+++ server/src/common/jwt/index.js
@@ -1,17 +1,15 @@
-const _             = require('lodash');
-const verifyToken   = require('./repository').verifyToken;
+const _ = require('lodash');
+const verifyToken = require('./repository').verifyToken;
 const generateToken = require('./repository').generateToken;
 
-const authenticate = (req, res, next) => {
-    return verifyToken(req.headers.authorization)
-        .then(payload => {
-            _.assign(req, { resources: { payload } });
-            next();
-        })
-        .catch(next);
-};
+const authenticate = (req, res, next) => verifyToken(req.headers.authorization)
+  .then((payload) => {
+    _.assign(req, { resources: { payload } });
+    next();
+  })
+  .catch(next);
 
 module.exports = {
-    authenticate,
-    generateToken
+  authenticate,
+  generateToken,
 };
