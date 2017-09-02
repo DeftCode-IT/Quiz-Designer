c78adfe3d113a694294f301cf06f49394d748799
MatexQ <mateuszxds@gmail.com>
2017-09-02 11:39:02 +0200

    Add winston and morgan

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const config = require('./config');
+const logger = require('./common/logger');
 
 const app = express();
+
+app.use(morgan('dev', { stream: { write: message => logger.info(message) } }));
+
 
 app.listen(config.http.port, () => console.log(`Server is working on port: ${config.http.port}`));
