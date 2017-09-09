3ece8d40be1b8731e8a53dbd44407445e611b920
MatexQ <mateuszxds@gmail.com>
2017-09-09 18:24:39 +0200

    Refactoring

--- server/src/helpers/index.js
+++ server/src/helpers/index.js
@@ -5,20 +5,7 @@ const resolve = (req, res) =>
     .status(_.result(res, 'result.status', 200))
     .json(_.omit(res.result || {}, ['status']));
 
-const error = err => {
-  const response = {
-    name: err.name,
-    message: err.message,
-  };
-
-  if (err.options) {
-    _.assign(response, { options: err.options });
-  }
-
-  return response;
-};
 
 module.exports = {
   resolve,
-  error,
 };
